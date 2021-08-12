12e89025114c5bfe25ed4695d3e70fe3e72ac1e5
leoSandrini <thesandrini@gmail.com>
2021-08-11 22:31:10 -0300

    starting an MERN application

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path")
+
+module.exports =  {
+    mode: "development",
+    entry: path.resolve(__dirname, `src`, `app`),
+    output:{
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devServer: {
+        historyApiFallback: true
+    }
+}
